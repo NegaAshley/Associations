539678e790ff5ef8d1c64da64e97d7810fe6f5e7
NegaAshley <ashleymxu@gmail.com>
2018-12-26 06:09:54 +0000

    Adding new post to test

--- references.js
+++ references.js
@@ -9,29 +9,29 @@ var User = require("./models/user");
 //     name: "Bob Belcher"
 // });
 
-// Post.create({
-//     title: "How to Cook the Best Burger Part 3",
-//     content: "adsfasdfasd asdf asdfasdf"
-// }, function(err, post){
-//     if(err){
-//         console.log(err);
-//     }else{
-//         User.findOne({email: "bob@gmail.com"}, function(err, foundUser){
-//             if(err){
-//                 console.log(err);
-//             }else{
-//                 foundUser.posts.push(post);
-//                 foundUser.save(function(err, data){
-//                   if(err){
-//                       console.log(err);
-//                   }else{
-//                       console.log(data);
-//                   }
-//                 });
-//             }
-//         });
-//     }
-// });
+Post.create({
+    title: "How to Cook the Best Burger Part 4",
+    content: "adsfasdfasd asdf asdfasdf adf afsasf"
+}, function(err, post){
+    if(err){
+        console.log(err);
+    }else{
+        User.findOne({email: "bob@gmail.com"}, function(err, foundUser){
+            if(err){
+                console.log(err);
+            }else{
+                foundUser.posts.push(post);
+                foundUser.save(function(err, data){
+                  if(err){
+                      console.log(err);
+                  }else{
+                      console.log(data);
+                  }
+                });
+            }
+        });
+    }
+});
 
 // //Find user
 // User.findOne({email: "bob@gmail.com"}).populate("posts").exec(function(err, user){
