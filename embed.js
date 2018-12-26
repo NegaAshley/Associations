def5b106699f1004214d2fdac61187d25c97e17d
NegaAshley <ashleymxu@gmail.com>
2018-12-26 04:46:46 +0000

    Add schema and model for user and post

--- embed.js
+++ embed.js
@@ -1,1 +1,18 @@
 var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/blog_demo");
+
+//User - email and name
+var userSchema = new mongoose.schema({
+    email: String,
+    name: String
+});
+
+var User = mongoose.model("User", userSchema);
+
+//Post - title, content
+var postSchema = new mongoose.schema({
+    title: String,
+    content: String
+});
+
+var Post = mongoose.model("Post", postSchema);
