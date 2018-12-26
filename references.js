088458cc64afbcdc04f76d1fb3de1d601640c874
NegaAshley <ashleymxu@gmail.com>
2018-12-26 05:46:15 +0000

    Create user with new schema

--- references.js
+++ references.js
@@ -13,9 +13,18 @@ var Post = mongoose.model("Post", postSchema);
 var userSchema = new mongoose.Schema({
     email: String,
     name: String,
-    posts: [postSchema]
+    posts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post"
+        }    
+    ]
 });
 
 var User = mongoose.model("User", userSchema);
 
 
+User.create({
+    email: "bob@gmail.com",
+    name: "Bob Belcher"
+});
